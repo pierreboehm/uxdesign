53ed208ef6c8116d854b659edacc69cccc9acddb
pierreboehm <pierre.boehm@web.de>
2021-04-24 10:17:58 +0200

    setup main components

--- app/src/main/java/org/pb/android/uxdesign/MainActivity.java
+++ app/src/main/java/org/pb/android/uxdesign/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         // TODO: uncomment
-        EventBus.getDefault().register(this);
+        //EventBus.getDefault().register(this);
     }
 
     @Override
--- app/src/main/java/org/pb/android/uxdesign/fragment/MainFragment.java
+++ app/src/main/java/org/pb/android/uxdesign/fragment/MainFragment.java
@@ -2,7 +2,9 @@ package org.pb.android.uxdesign.fragment;
 
 import androidx.fragment.app.Fragment;
 
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EFragment;
+import org.pb.android.uxdesign.Demonstrator;
 import org.pb.android.uxdesign.R;
 
 @EFragment(R.layout.fragment_main)
@@ -10,5 +12,16 @@ public class MainFragment extends Fragment {
 
     public static final String TAG = MainFragment.class.getSimpleName();
 
+    @Bean
+    Demonstrator demonstrator;
 
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
 }
