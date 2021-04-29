ae356598600a2b8d900d3618fa527d4381a41f06
pierreboehm <pierre.boehm@web.de>
2021-04-29 11:01:16 +0200

    add util methods

--- app/src/main/java/org/pb/android/uxdesign/util/Util.java
+++ app/src/main/java/org/pb/android/uxdesign/util/Util.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 
+import java.util.Random;
+
 public class Util {
 
     public static float convertDpToPx(Context context, float valueInDp) {
@@ -21,4 +23,26 @@ public class Util {
         return valueInPx / scaledDensity;
     }
 
+    public static double roundScale(double d) {
+        return Math.rint(d * 100) / 100.;
+    }
+
+    public static float getCardiacRandom() {
+        // 0.0 --> 1.0      r/10 = 0.00 .. 0.1  r/100 = 0.000 .. 0.01
+        return (float) Math.random() / 10f;
+    }
+
+    // TODO: also works with negatives?
+    public static double getRandomBetween(double min, double max) {
+        return min + (max - min) * new Random().nextDouble();
+    }
+
+    // TODO: also works with negatives?
+    public static float getRandomBetween(float min, float max) {
+        return min + (max - min) * new Random().nextFloat();
+    }
+
+    public static int getRandomBetween(int min, int max) {
+        return min + (int) (Math.random() * ((max - (min)) + 1));
+    }
 }
