344bd533281a1dcc75297dfb1aece9c765332c73
pierreboehm <pierre.boehm@web.de>
2021-05-01 12:32:13 +0200

    basic implementation of a view

---
+++ app/src/main/java/org/pb/android/uxdesign/ui/button/ButtonView.java
@@ -0,0 +1,29 @@
+package org.pb.android.uxdesign.ui.button;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import org.androidannotations.annotations.EViewGroup;
+import org.androidannotations.annotations.ViewById;
+import org.pb.android.uxdesign.R;
+
+@SuppressLint("NonConstantResourceId")
+@EViewGroup(R.layout.button_open_hexagon_with_dot)
+public class ButtonView extends LinearLayout {
+
+    @ViewById(R.id.tvText)
+    TextView tvText;
+
+    public ButtonView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setText(String text) {
+        tvText.setText(text);
+    }
+}
