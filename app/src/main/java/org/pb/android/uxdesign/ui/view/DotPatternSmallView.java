412497911d5d116fadd8a2d02da99db1eb885bdb
pierreboehm <pierre.boehm@web.de>
2021-05-02 11:08:15 +0200

    implement end embed dot pattern small view

---
+++ app/src/main/java/org/pb/android/uxdesign/ui/view/DotPatternSmallView.java
@@ -0,0 +1,61 @@
+package org.pb.android.uxdesign.ui.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EView;
+import org.pb.android.uxdesign.R;
+import org.pb.android.uxdesign.util.Util;
+
+@EView
+public class DotPatternSmallView extends View {
+
+    private Paint color;
+
+    public DotPatternSmallView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @AfterViews
+    public void initView() {
+        setWillNotDraw(false);
+
+        color = new Paint();
+        color.setColor(getContext().getColor(R.color.blue_deep_sky));
+        color.setStyle(Paint.Style.FILL);
+
+        invalidate();
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        drawDotPattern(canvas);
+
+        super.onDraw(canvas);
+    }
+
+    private void drawDotPattern(Canvas canvas) {
+        int width = canvas.getWidth();
+        int dotCountX = 4;
+
+        float square = (float) width / (float) (dotCountX + 1);
+        float quarterSquare = square / (float) dotCountX;
+
+        for (int dotX = 0; dotX < dotCountX; dotX++) {
+
+            float dotXPosition = dotX * (square + quarterSquare);
+            int dotCountY = Util.getRandomBetween(1, dotCountX);
+
+            for (int dotY = 0; dotY < dotCountY; dotY++) {
+                float dotYPosition = dotY * (square + quarterSquare);
+                canvas.drawRect(dotXPosition, dotYPosition, dotXPosition + square, dotYPosition + square, color);
+            }
+        }
+    }
+}
