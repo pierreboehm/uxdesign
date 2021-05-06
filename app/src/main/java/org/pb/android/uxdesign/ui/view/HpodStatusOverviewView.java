f9071a533679544b8c49d2f0c6f722363df44370
pierreboehm <pierre.boehm@web.de>
2021-05-06 17:07:53 +0200

    implement hpod status overview view

---
+++ app/src/main/java/org/pb/android/uxdesign/ui/view/HpodStatusOverviewView.java
@@ -0,0 +1,19 @@
+package org.pb.android.uxdesign.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+import org.androidannotations.annotations.EViewGroup;
+import org.pb.android.uxdesign.R;
+
+@EViewGroup(R.layout.view_hpod_status_overview)
+public class HpodStatusOverviewView extends LinearLayout {
+
+    public HpodStatusOverviewView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
