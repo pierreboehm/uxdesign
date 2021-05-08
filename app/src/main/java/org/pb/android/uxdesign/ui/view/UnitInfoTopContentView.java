2cf67619b800e84c98e6e527f82e5b489ec45529
pierreboehm <pierre.boehm@web.de>
2021-05-08 11:35:23 +0200

    implement unit info top content view

---
+++ app/src/main/java/org/pb/android/uxdesign/ui/view/UnitInfoTopContentView.java
@@ -0,0 +1,16 @@
+package org.pb.android.uxdesign.ui.view;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+import org.androidannotations.annotations.EViewGroup;
+import org.pb.android.uxdesign.R;
+
+@EViewGroup(R.layout.view_unit_info_top_content)
+public class UnitInfoTopContentView extends RelativeLayout {
+
+    public UnitInfoTopContentView(Context context) {
+        super(context);
+    }
+
+}
