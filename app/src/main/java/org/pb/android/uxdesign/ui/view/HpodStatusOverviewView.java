d4b076c45b945bd9987990d943882d83801d5b8f
pierreboehm <pierre.boehm@web.de>
2021-05-08 12:49:48 +0200

    perform some small layout adaptions

--- app/src/main/java/org/pb/android/uxdesign/ui/view/HpodStatusOverviewView.java
+++ app/src/main/java/org/pb/android/uxdesign/ui/view/HpodStatusOverviewView.java
@@ -1,5 +1,6 @@
 package org.pb.android.uxdesign.ui.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
@@ -9,11 +10,11 @@ import androidx.annotation.Nullable;
 import org.androidannotations.annotations.EViewGroup;
 import org.pb.android.uxdesign.R;
 
+@SuppressLint("NonConstantResourceId")
 @EViewGroup(R.layout.view_hpod_status_overview)
 public class HpodStatusOverviewView extends LinearLayout {
 
     public HpodStatusOverviewView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
-
 }
