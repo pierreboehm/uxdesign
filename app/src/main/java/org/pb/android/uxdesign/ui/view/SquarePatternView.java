158b5ea9f2f8f2b7e6c1ebf3891453c19acfdaa9
pierreboehm <pierre.boehm@web.de>
2021-05-12 11:15:20 +0200

    implement square pattern view

---
+++ app/src/main/java/org/pb/android/uxdesign/ui/view/SquarePatternView.java
@@ -0,0 +1,70 @@
+package org.pb.android.uxdesign.ui.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EView;
+import org.androidannotations.annotations.UiThread;
+import org.pb.android.uxdesign.R;
+import org.pb.android.uxdesign.util.Util;
+
+@EView
+public class SquarePatternView extends View {
+
+    private Paint color;
+
+    public SquarePatternView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @AfterViews
+    public void initView() {
+        setWillNotDraw(false);
+
+        color = new Paint();
+        color.setColor(getContext().getColor(R.color.blue_deep_sky));
+        color.setStyle(Paint.Style.FILL);
+
+        invalidate();
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        drawPattern(canvas);
+        super.onDraw(canvas);
+    }
+
+    @UiThread
+    public void update() {
+        invalidate();
+    }
+
+    public void setDefaultColor(int defaultColor) {
+        color.setColor(defaultColor);
+    }
+
+    private void drawPattern(Canvas canvas) {
+        int width = canvas.getWidth();
+        int squareCountXY = 2;
+
+        float square = (float) width / (float) (squareCountXY + 1);
+        float quarterSquare = square / 4f;
+
+        for (int squareX = 0; squareX < squareCountXY; squareX++) {
+
+            float squareXPosition = squareX * (square + quarterSquare);
+
+            for (int squareY = 0; squareY < squareCountXY; squareY++) {
+                float dotYPosition = squareY * (square + quarterSquare);
+                color.setAlpha(Util.getRandomBoolean() ? 255 : 96);
+                canvas.drawRect(squareXPosition, dotYPosition, squareXPosition + square, dotYPosition + square, color);
+            }
+        }
+    }
+}
