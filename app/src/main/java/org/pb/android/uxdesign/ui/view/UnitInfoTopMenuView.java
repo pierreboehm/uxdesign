a0ce12eb6545f68bbd5123f13c975d1d04c3303d
pierreboehm <pierre.boehm@web.de>
2021-05-13 15:29:09 +0200

    implement unit info top menu button selected blink animation

--- app/src/main/java/org/pb/android/uxdesign/ui/view/UnitInfoTopMenuView.java
+++ app/src/main/java/org/pb/android/uxdesign/ui/view/UnitInfoTopMenuView.java
@@ -1,14 +1,16 @@
 package org.pb.android.uxdesign.ui.view;
 
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
+
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 
-import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EViewGroup;
 import org.androidannotations.annotations.ViewsById;
@@ -22,6 +24,10 @@ import java.util.List;
 @SuppressLint("NonConstantResourceId")
 @EViewGroup(R.layout.view_unit_info_menue_top)
 public class UnitInfoTopMenuView extends RelativeLayout {
+
+    private static final long ANIMATION_SPEED = 1000L;
+    private static final int ALPHA_MIN = 96;
+    private static final int ALPHA_MAX = 255;
 
     @ViewsById({R.id.btn_0, R.id.btn_1, R.id.btn_2, R.id.btn_3, R.id.btn_4, R.id.btn_5})
     List<ImageButton> buttonGroup1List;
@@ -41,14 +47,28 @@ public class UnitInfoTopMenuView extends RelativeLayout {
     @Pref
     AppPreferences_ preferences;
 
+    private ValueAnimator animator;
+    private ImageButton lastButtonSelected;
+
     public UnitInfoTopMenuView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    @AfterViews
-    public void initView() {
-        // preselect 'sustainment info' from preferences
-        performSelection(buttonGroup1List.get(preferences.selectedButton().get()));
+    @Override
+    public void onVisibilityChanged(View changedView, int visibility) {
+        super.onVisibilityChanged(changedView, visibility);
+
+        if (visibility == VISIBLE) {
+            performSelection(buttonGroup1List.get(preferences.selectedButton().get()));
+        } else {
+            hideSelectedStatus();
+        }
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        hideSelectedStatus();
+        super.onDetachedFromWindow();
     }
 
     @Click({R.id.btn_0, R.id.btn_1, R.id.btn_2, R.id.btn_3, R.id.btn_4, R.id.btn_5})
@@ -71,9 +91,15 @@ public class UnitInfoTopMenuView extends RelativeLayout {
         }
 
         // store preselection in preferences
-        int indexOfMainButtonClicked = indexOf(button.getId());
+        int indexOfMainButtonClicked = indexOf(button.getId());         // identity only main-buttons!
         if (indexOfMainButtonClicked != -1) {
             preferences.selectedButton().put(indexOfMainButtonClicked);
+
+            hideSelectedStatus();
+            if (button.isSelected()) {
+                showSelectedStatus(ALPHA_MAX, ALPHA_MIN, button);
+                lastButtonSelected = button;
+            }
         }
     }
 
@@ -90,6 +116,7 @@ public class UnitInfoTopMenuView extends RelativeLayout {
             if (skipId == null || imageButton.getId() != skipId) {
                 imageButton.setSelected(false);
             }
+            imageButton.setImageAlpha(ALPHA_MAX);
         }
     }
 
@@ -101,5 +128,34 @@ public class UnitInfoTopMenuView extends RelativeLayout {
             }
         }
         return -1;
+    }
+
+    private void showSelectedStatus(int startValue, int endValue, ImageButton imageButton) {
+
+        if (lastButtonSelected != null && lastButtonSelected.getId() != imageButton.getId()) {
+            lastButtonSelected.setImageAlpha(ALPHA_MAX);
+        }
+
+        animator = ValueAnimator.ofInt(startValue, endValue);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int alpha = (int) animation.getAnimatedValue();
+                imageButton.setImageAlpha(alpha);
+            }
+        });
+
+        animator.setDuration(ANIMATION_SPEED);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+        animator.start();
+    }
+
+    private void hideSelectedStatus() {
+        if (animator != null) {
+            animator.end();
+            animator.cancel();
+            animator = null;
+        }
     }
 }
